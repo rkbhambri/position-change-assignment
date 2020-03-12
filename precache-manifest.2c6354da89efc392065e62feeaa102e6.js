3def15993bf331ecfbad537c83843c433c6a0955
rkbhambri <ramkrishan@redbaron.in>
2020-03-12 17:12:47 +0530

    Updates

---
+++ precache-manifest.2c6354da89efc392065e62feeaa102e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72f90b34d4f72d2dbce77830dd81dd2",
+    "url": "/position-change-assignment/index.html"
+  },
+  {
+    "revision": "5f5636aa8df61bde868d",
+    "url": "/position-change-assignment/static/css/main.cb51e1bf.chunk.css"
+  },
+  {
+    "revision": "faf87fb41aba3956f672",
+    "url": "/position-change-assignment/static/js/2.5f465522.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/position-change-assignment/static/js/2.5f465522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5636aa8df61bde868d",
+    "url": "/position-change-assignment/static/js/main.4b7322a0.chunk.js"
+  },
+  {
+    "revision": "20627c0b6c7935007c60",
+    "url": "/position-change-assignment/static/js/runtime-main.f407fb38.js"
+  }
+]);
